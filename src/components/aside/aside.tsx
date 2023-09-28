f6dea167c4fd7699a026a8a13da5cb14f6e025b8
Chris Haskins <chris@xeelee.org>
2023-09-28 12:21:08 -0700

    Add types to Aside (info, warning).

--- src/components/aside/aside.tsx
+++ src/components/aside/aside.tsx
@@ -1,10 +1,25 @@
 import React, { PropsWithChildren } from "react"
+import InfoIcon from "../icons/info-icon"
+import WarningIcon from "../icons/warning-icon"
 import * as styles from "./aside.module.scss"
 
-const Aside = ({ children }: PropsWithChildren): JSX.Element => {
+type AsideProps = {
+    type: "info" | "warning"
+}
+
+const Aside = ({ children, type = 'info' }: PropsWithChildren<AsideProps>): JSX.Element => {
+
+    const icon = type === "info" ? <InfoIcon /> : <WarningIcon />;
+    const asideContainerClass = type === "info" ? styles.infoContainer : styles.warningContainer;
+
     return (
-        <div className={styles.asideContainer}>
-            {children}
+        <div className={asideContainerClass}>
+            <div className={styles.iconContainer}>
+                {icon}
+            </div>
+            <div className={styles.textContainer}>
+                {children}
+            </div>
         </div>
     )
 }
