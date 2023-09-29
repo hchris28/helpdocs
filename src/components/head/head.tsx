ec72fc34c97038e841ff201ced554478da0eb37a
Chris Haskins <chris@xeelee.org>
2023-09-29 12:05:17 -0700

    Add user name to toolar and company name to title.

--- src/components/head/head.tsx
+++ src/components/head/head.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
-import type { HeadFC, PageProps } from "gatsby"
+import type { HeadFC } from "gatsby"
+import { useReadLocalStorage } from 'usehooks-ts'
 
 type HeadContext = {
     company: string
 }
 
 export const Head: HeadFC<null, HeadContext> = ({ pageContext: { company } }) => {
+
+    const user = useReadLocalStorage<User | undefined>('user');
+
     return (
         <>
             <html lang="en" />
-            <title>{company} Help Docs</title>
+            <title>{user?.company.name} Help Docs</title>
             <link rel="preconnect" href="https://fonts.googleapis.com" />
             <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
             <link href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap" rel="stylesheet" />
--- src/components/tool-bar/tool-bar.tsx
+++ src/components/tool-bar/tool-bar.tsx
@@ -1,10 +1,15 @@
 import React from "react"
+import { useReadLocalStorage } from 'usehooks-ts'
 import LogOutButton from '../../components/log-out-button/log-out-button'
 import * as styles from "./tool-bar.module.scss"
 
 const ToolBar = (): JSX.Element => {
+
+    const user = useReadLocalStorage<User | undefined>('user');
+
     return (
         <div className={styles.container}>
+            <div className={styles.userName}>Hi {user?.name}</div>
             <LogOutButton />
         </div>
     )
