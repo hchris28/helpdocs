881bda029c09dfd44f5aa04ff0e9481f9958878b
Chris Haskins <chris@xeelee.org>
2023-10-02 22:37:17 -0700

    Add AnimGif component

--- src/components/document/document.tsx
+++ src/components/document/document.tsx
@@ -1,9 +1,11 @@
 import React, { PropsWithChildren } from "react"
 import { MDXProvider } from "@mdx-js/react"
 import Aside from "../aside/aside";
+import AnimGif from "../anim-gif/anim-gif";
 
 const shortcodes = {
     Aside,
+    AnimGif,
 };
 
 const Document = ({ children }: PropsWithChildren): JSX.Element => {
